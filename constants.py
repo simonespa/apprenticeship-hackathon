3836924320f612919086d917d5ed67dc80fa3f46
Raymond Wong <raymond.wong@i4-insight.com>
2024-02-14 12:35:40 +0000

    Add constants and plotting file

---
+++ constants.py
@@ -0,0 +1,21 @@
+DATETIME_FEATURE = "test_date"
+
+COUGH = "cough"
+FEVER = "fever"
+SORE_THROAT = "sore_throat"
+SHORTNESS_OF_BREATH = "shortness_of_breath"
+HEADACHE = "head_ache"
+AGE_60_AND_ABOVE = "age_60_and_above"
+TEST_INDICATION = "test_indication"
+
+TARGET = "corona_result"
+
+CATEGORICAL_FEATURES = [
+    AGE_60_AND_ABOVE,
+    COUGH,
+    FEVER,
+    HEADACHE,
+    SHORTNESS_OF_BREATH,
+    SORE_THROAT,
+    TEST_INDICATION
+]
