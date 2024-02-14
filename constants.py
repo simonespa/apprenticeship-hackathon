266d3c8406571edd46f7f7e5ea1090eb5171d087
Raymond Wong <raymond.wong@i4-insight.com>
2024-02-14 12:35:40 +0000

    Add constants and plotting file

---
+++ constants.py
@@ -0,0 +1,12 @@
+DATETIME_FEATURE = "test_date"
+
+AGE_60_AND_ABOVE = "age_60_and_above"
+COUGH = "cough"
+FEVER = "fever"
+GENDER = "gender"
+HEADACHE = "head_ache"
+SORE_THROAT = "sore_throat"
+SHORTNESS_OF_BREATH = "shortness_of_breath"
+TEST_INDICATION = "test_indication"
+
+TARGET = "corona_result"
---
+++ plotting.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from constants import DATETIME_FEATURE, TARGET
+
+sns.set_style("whitegrid")
+
+
+def plot_features(feature: str, feature_data: pd.Series):
+    feature_data.value_counts().plot(kind='bar')
+
+    plt.title(f'{feature} Distribution')
+    plt.xlabel(feature)
+    plt.ylabel('Frequency')
+    plt.show()
+
+
+def plot_timeline(data: pd.DataFrame, resample_parameter: str = "W"):
+    data[DATETIME_FEATURE] = pd.to_datetime(data[DATETIME_FEATURE])
+    data = data.set_index(DATETIME_FEATURE)
+
+    results = data.resample(resample_parameter)[TARGET].value_counts().unstack(fill_value=0)
+    results.index = results.index.strftime('%Y-%m-%d')
+
+    results.plot(kind='bar', stacked=True, figsize=(16, 8))
+    plt.title('Timeline of COVID-19 Test Results')
+    plt.xlabel('Date')
+    plt.ylabel('Number of Tests Conducted')
+    plt.xticks(rotation=45)
+    plt.legend(title='Diagnosis')
+    plt.show()
